a169450af54ade235e1c0333061f835ca73ac351
moneymatt7 <107777036+moneymatt7@users.noreply.github.com>
2024-03-21 20:27:17 -0400

    Latest version, minor changes

---
+++ JavaDemo1.0/src/edu/fscj/JavaDemo/main/Main.java
@@ -0,0 +1,52 @@
+package edu.fscj.JavaDemo.main;
+
+import edu.fscj.JavaDemo.art.Letters.*;
+import java.util.Scanner;
+
+import static edu.fscj.JavaDemo.art.Letters.ASCII_CHAR_MAP;
+
+public class Main {
+
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        // prompt the user to enter their first name
+        System.out.println("Please enter your first name:");
+        String input = sc.nextLine().toUpperCase();
+
+        System.out.print("Welcome, \n" + toAscii(input) + "\nThis is a small " +
+                "demo application brought to you by the \n" +
+                toAscii("FSCJ STARS!"));
+
+    }
+
+    public static StringBuilder toAscii(String input) {
+        // row height of each letter
+        final int LETTER_HEIGHT = 5;
+
+        StringBuilder line = new StringBuilder();
+
+        // iterate over each row
+        for (int row = 0; row < LETTER_HEIGHT; row++) {
+
+            for (char c : input.toCharArray()) {
+                // get ASCII art for current char
+                String[] asciiLetter = ASCII_CHAR_MAP.get(c);
+
+                // check map for character, append it to the line
+                if (asciiLetter != null && row < asciiLetter.length) {
+                    line.append(asciiLetter[row]);
+                    line.append(" ");
+                }else{
+                    // if letter not in map append space
+                    line.append("       ");
+                }
+            }
+            line.append("\n");
+        }
+
+        return line;
+    }
+}
+
